bad09ef5a56f68077b441a57139837d0486bc46f
Shahzishaheer <ashaheer727@gmail.com>
2025-10-30 02:11:58 +0500

    add input otp

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+
+// src/lib/utils.ts
+// Utility to merge class names (for Tailwind/React)
+export function cn(...classes: Array<string | undefined | null | false>) {
+  return classes.filter(Boolean).join(' ');
 }
